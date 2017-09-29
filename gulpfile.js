8bef2dac07e9d8c89e82ae10ebb821630804e643
Franz Schnyder <schnyderfranz@gmail.com>
2017-09-29 22:56:22 +0200

    Fix host build on travis

--- gulpfile.js
+++ gulpfile.js
@@ -166,5 +166,5 @@ gulp.task('flash-online', function(cb){
 
 
 gulp.task('build',['compile-local','fw:host:test']);
-gulp.task('travis', ['compile-online', 'fw:host:test']);
+gulp.task('travis', plugins.sequence('install-ext', [ 'compile-online', 'fw:host:test']));
 gulp.task('default', ['build']);
