5917e561cec364d4324a5751e4d1c6aa61448c55
Franz Schnyder <schnyderfranz@gmail.com>
2018-08-02 11:45:51 +0200

    Change MessageBufferSink impl to use a counter instead of empty entry.

--- fw/lib/BtCore/src/Bt/SolarMonitor/MessageBufferSink.h
+++ fw/lib/BtCore/src/Bt/SolarMonitor/MessageBufferSink.h
@@ -20,13 +20,13 @@ template<size_t SIZE>
 class MessageBufferSink : public I_MessageBuffer
 {
    private:
-      static const size_t BUFFER_SIZE = SIZE + 1;
+      static const size_t BUFFER_SIZE = SIZE;
 
    public:
       static const size_t MESSAGE_BUFFER_LENGHT = 256;
 
       MessageBufferSink()
-      : mWriteIndex(0), mReadIndex(0)
+      :mCount(0), mWriteIndex(0), mReadIndex(0)
       {}
       MessageBufferSink(const MessageBufferSink&) = delete;
       MessageBufferSink& operator=(const MessageBufferSink&) = delete;
@@ -37,10 +37,13 @@ class MessageBufferSink : public I_MessageBuffer
       void consume(const char* pMessage){
          strncpy(mStorage[mWriteIndex], pMessage, MESSAGE_BUFFER_LENGHT);
          mWriteIndex = (mWriteIndex + 1) % BUFFER_SIZE;
-         if(mWriteIndex == mReadIndex)
+         if(mCount == SIZE)
          {
             BT_CORE_LOG_WARN("MessageBufferSink override buffered unsent message!");
             mReadIndex = (mReadIndex + 1) % BUFFER_SIZE;
+         }
+         else {
+            mCount++;
          }
          BT_CORE_LOG_INFO("MessageBufferSink appended %d [%d|%d]", forPrintf(count()), forPrintf(mReadIndex), forPrintf(mWriteIndex)  );
       }
@@ -61,24 +64,23 @@ class MessageBufferSink : public I_MessageBuffer
             return;
          }
          mReadIndex = (mReadIndex + 1) % BUFFER_SIZE;
+         mCount--;
          BT_CORE_LOG_INFO("MessageBufferSink removed %d [%d|%d]", forPrintf(count()), forPrintf(mReadIndex), forPrintf(mWriteIndex)  );
       }
 
       virtual bool empty() const
       {
-         return mWriteIndex == mReadIndex;
+         return mCount == 0;
       }
 
       virtual size_t count() const {
-         if(mWriteIndex >= mReadIndex) {
-            return mWriteIndex - mReadIndex;
-         }
-         return mWriteIndex + BUFFER_SIZE - mReadIndex;
+         return mCount;
       }
 
 
    private:
       std::array<char[MESSAGE_BUFFER_LENGHT], BUFFER_SIZE> mStorage;
+      size_t mCount;
       size_t mWriteIndex;
       size_t mReadIndex;
 
